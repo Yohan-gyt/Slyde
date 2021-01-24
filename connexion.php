22ed8167d6c351d352d6191174f8643a937cc524
Yohan Guyot <61965649+Yohan-gyt@users.noreply.github.com>
2021-01-24 07:48:59 +0100

    Accueil fini + nettoyage code et modif
    - personnalisation de la checkbox qui marche
    - les input séparés en deux font vraiment la même taille
    -flèche svg pour le retour à l'accueil (la même que la maquette)
    - j'ai enlevé la possibilité de séléctionner des trucs (ça fait plus propre)
    - J'ai renommé les classes genre wrapper en wrapper-qqchose au cas ou on ai besoin d'autres wrappers
    - J'ai ajouté des hover
    - j'ai modifié des margins pour que ça soit plus cohérent avec la page d'accueil et la maquette
    - il est 7h48 je vais dodo (bonne nuit)

--- connexion.php
+++ connexion.php
@@ -9,14 +9,14 @@
     <meta name="description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
 
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://slyde.fr/index.html">
-    <meta property="og:title" content="SLYDE - Créer, collaborer, partager">
+    <meta property="og:url" content="https://slyde.fr/connexion.php">
+    <meta property="og:title" content="SLYDE - Connexion">
     <meta property="og:description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
     <meta property="og:image" content="https://www.slyde.fr/assets/img/card/card.jpg">
 
     <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://slyde.fr/index.html">
-    <meta property="twitter:title" content="SLYDE - Créer, collaborer, partager">
+    <meta property="twitter:url" content="https://slyde.fr/connexion.php">
+    <meta property="twitter:title" content="SLYDE - Connexion">
     <meta property="twitter:description"
         content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
     <meta property="twitter:image" content="https://www.slyde.fr/assets/card/card.jpg">
@@ -24,35 +24,51 @@
     <title>SLYDE - Connexion</title>
 
     <link rel="stylesheet" href="assets/css/styles.css">
+
     <link rel="icon" href="assets/img/favicon.ico">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;800&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
-    <link rel="apple-touch-icon" sizes="120x120" href="assets/card/apple-touch-icon-120x120-precomposed.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="assets/card/apple-touch-icon-152x152-precomposed.png">
+
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/card/apple-touch-icon-120x120-precomposed.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/card/apple-touch-icon-152x152-precomposed.png">
 </head>
 
 <body>
-    <div class="wrapper">
+    <div class="wrapper-sign">
         <section id="bg-left">
-            <h1 class="title">Connexion</h1>
+            <h1 class="title">CONNEXION</h1>
         </section>
 
         <section id="signin">
             <nav class="header-signin">
-                <a href="index.php"><span class="yellow">←</span> Retour à l'accueil</a>
-                <p>Pas encore membre ? <a href="signup-personalinfo.php"><span class="yellow">S'inscrire<span></a></p>
+                <a href="index.html" class="back-home">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="41" height="22.121" viewBox="0 0 41 22.121" class="arrow">
+                        <g id="Group_23" data-name="Group 23" transform="translate(1.5 1.061)">
+                            <line id="Line_161" data-name="Line 161" x1="39" transform="translate(0.5 10.5)" fill="none" stroke="#fee12e" stroke-width="3"/>
+                            <path id="Path_2" data-name="Path 2" d="M0,0,10,10,20,0" transform="translate(10) rotate(90)" fill="none" stroke="#fee12e" stroke-linejoin="round" stroke-width="3"/>
+                        </g>
+                    </svg>
+                    <div class="back-text">
+                        Retour à l'accueil
+                    </div>
+                </a>
+                <p> Pas encore membre ? 
+                    <a href="inscription.php" class="link-sign">
+                        S'inscrire
+                    </a>
+                </p>
             </nav>
 
             <div class="wrapper-right">
                 <h2>Connexion à mon compte Slyde</h2>
                 <hr>
                 <form action="assets/php/" method="POST" class="form-singin">
-                    <input type="email" name="email" placeholder="johndoe@exemple.fr" required>
+                    <input type="email" name="email" placeholder="Email" required>
                     <input type="password" name="password" placeholder="Mot de passe" required>
                     <a href="#" class="forgot-pwd">Mot de passe oublié ?</a>
                     <button type="submit">Se connecter</button>
@@ -60,7 +76,6 @@
             </div>
         </section>
     </div>
-
 </body>
 
 </html>
--- inscription.php
+++ inscription.php
@@ -9,19 +9,19 @@
     <meta name="description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
 
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://slyde.fr/index.html">
-    <meta property="og:title" content="SLYDE - Créer, collaborer, partager">
+    <meta property="og:url" content="https://slyde.fr/inscription.php">
+    <meta property="og:title" content="SLYDE - Inscription">
     <meta property="og:description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
     <meta property="og:image" content="https://www.slyde.fr/assets/img/card/card.jpg">
 
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://slyde.fr/index.html">
-    <meta property="twitter:title" content="SLYDE - Créer, collaborer, partager">
+    <meta property="twitter:title" content="SLYDE - Inscription">
     <meta property="twitter:description"
         content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
     <meta property="twitter:image" content="https://www.slyde.fr/assets/card/card.jpg">
 
-    <title>SLYDE - Connexion</title>
+    <title>SLYDE - Inscription</title>
 
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="icon" href="assets/img/favicon.ico">
@@ -37,29 +37,50 @@
 </head>
 
 <body>
-    <div class="wrapper">
+    <div class="wrapper-sign">
         <section id="bg-left">
-            <h1 class="title">Mes informations personnelles</h1>
+            <h1 class="title">MES INFORMATIONS <br> PERSONNELLES</h1>
             <p class="numbers">01-04</p>
         </section>
 
         <section id="signup">
             <nav class="header-signin">
-                <a href="index.php"><span class="yellow">←</span> Retour à l'accueil</a>
-                <p>Déjà membre ?<a href="signin.php"><span class="yellow"> Se connecter<span></a></p>
+                <a href="index.html" class="back-home">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="41" height="22.121" viewBox="0 0 41 22.121" class="arrow">
+                        <g id="Group_23" data-name="Group 23" transform="translate(1.5 1.061)">
+                            <line id="Line_161" data-name="Line 161" x1="39" transform="translate(0.5 10.5)" fill="none" stroke="#fee12e" stroke-width="3"/>
+                            <path id="Path_2" data-name="Path 2" d="M0,0,10,10,20,0" transform="translate(10) rotate(90)" fill="none" stroke="#fee12e" stroke-linejoin="round" stroke-width="3"/>
+                        </g>
+                    </svg>
+                    <div class="back-text">
+                        Retour à l'accueil
+                    </div>
+                </a>
+                <p>Déjà membre ?
+                    <a href="connexion.php" class="link-sign">
+                         Se connecter
+                    </a>
+                </p>
             </nav>
             <div class="wrapper-right">
                 <form action="assets/php/" method="POST" class="form-signup">
-                    <input type="text" name="lastname" placeholder="Nom" required>
-                    <input type="text" name="name" placeholder="Prénom" required>
+                    <div class="separate">
+                        <input type="text" name="lastname" placeholder="Nom" required>
+                        <input type="text" name="name" placeholder="Prénom" required>
+                    </div>
                     <input type="text" name="username" placeholder="Nom d'utilisateur" required>
                     <input type="email" name="email" placeholder="Email" required>
                     <input type="text" name="location" placeholder="Localisation" required>
-                    <input type="password" name="pwd" placeholder="Mot de passe" required>
-                    <input type="password" name="pwd" placeholder="Confirmer le mot de passe" required>
+                    <div class="separate">
+                        <input type="password" name="pwd" placeholder="Mot de passe" required>
+                        <input type="password" name="pwd" placeholder="Confirmer le mot de passe" required>
+                    </div>
                 </form>
-                <label for="conditions">
-                    <input type="checkbox" name="conditions">J’accepte les Conditions générales d’utilisation
+                
+                <label class="conditions">
+                    <input type="checkbox" name="conditions">
+                    <span class="checkmark"></span>
+                    J’accepte les Conditions générales d’utilisation
                 </label>
             </div>
             <button type="submit" class="nextbutton">Suivant</button>
