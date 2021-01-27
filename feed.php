847e23c715494a66aedd70e2161a5bfd5e74135f
Rachel Albersario <61965598+RachelAlbs@users.noreply.github.com>
2021-01-27 17:56:08 +0100

    Bon j'ai aps du tout fini mais vsy j'ai faim

---
+++ feed.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" lang="fr"
+        content="Rachel Albersario, Thanh-Trang Luu, Jules Cointrel, Paul Barbosa & Yohan Guyot">
+    <meta name="description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://slyde.fr/connexion.php">
+    <meta property="og:title" content="SLYDE - Connexion">
+    <meta property="og:description" content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
+    <meta property="og:image" content="https://www.slyde.fr/assets/img/card/card.jpg">
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://slyde.fr/connexion.php">
+    <meta property="twitter:title" content="SLYDE - Connexion">
+    <meta property="twitter:description"
+        content="Bienvenue sur Slyde, la première plateforme de collaboration musicale !">
+    <meta property="twitter:image" content="https://www.slyde.fr/assets/card/card.jpg">
+
+    <title>SLYDE - Feed</title>
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <link rel="icon" href="assets/img/favicon.ico">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/card/apple-touch-icon-120x120-precomposed.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/card/apple-touch-icon-152x152-precomposed.png">
+</head>
+
+<body>
+    <section id="feed">
+        <div>
+            <img class="logo fixed" src="assets/img/logos/logo.png" alt="" draggable="false">
+        </div>
+        <nav class="navbar">
+            <!-- <input type="search" placeholder="un projet, un artiste, un instrument ..." name="the_search"> -->
+            <button class="create-project">Créer un projet</button>
+        </nav>
+
+        <div class="wrapper-feed">
+            <!-- REPRENDRE LES PROJETS -->
+            <h3>Reprendre mes projets</h3>
+
+            <div class="wrapper-card">
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- NOUVEAUX PROJETS -->
+            <h3>Nouveaux projets</h3>
+            <div class="wrapper-card">
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper-card">
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <img class="card" src="assets/img/background/pexels-breakingpic-3094.jpg" alt="">
+                    <div class="card-desc">
+                        <h4><span class="yellow">Pop & RnB</span></h4>
+                        <h4 class="card-title">Nom du projet</h4>
+                        <p class="user">John Doe</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+
+    <footer>
+        <div class="container-footer">
+            <div class="wrapper-left-footer">
+                <img src="assets/img/logos/logo-large.png" alt="" class="logo-2" draggable="false">
+                <p class="slogan-3">Créer, collaborer, partager.</p>
+                <div class="icons-footer">
+                    <a href="https://www.instagram.com/slyde.fr/" target="_blank" class="insta">
+                        <img class="link-icon" src="assets/img/icons/instagram.svg" alt="instagram" draggable="false">
+                    </a>
+                    <a href="https://www.linkedin.com/company/slydefr/" target="_blank" class="linkedin">
+                        <img class="link-icon" src="assets/img/icons/linkedin.svg" alt="linkedin" draggable="false">
+                    </a>
+                </div>
+            </div>
+            <div class="wrapper-right-footer">
+                <div class="contact">
+                    <p>Nous contacter</p>
+                    <a href="mailto:contact@slyde.fr">contact@slyde.fr</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="separator"></div>
+
+        <p class="copyright"> © 2020 Slyde, tous droits réservés</p>
+        <div class="wrapper-logo">
+            <img class="logo-footer" src="assets/img/logos/logo.png" alt="" draggable="false">
+            <p>Paris, France</p>
+            <p class="copyright-mobile"> © 2020 Slyde, tous droits réservés</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
